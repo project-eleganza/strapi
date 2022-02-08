d0b722d19fa4d1835d9974c6137069639800fa6e
Maria'lena Pietri <marialena.pietri@maps-system.com>
2022-02-08 19:31:14 +0100

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,24 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'aws-s3',
+    providerOptions: {
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
+      region: env('AWS_REGION'),
+      params: {
+        Bucket: env('AWS_BUCKET_NAME'),
+      },
+    }
+  },
+  email: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: env('MAIL_FROM_EMAIL'),
+        defaultReplyTo: env('MAIL_REPLY_EMAIL'),
+        testAddress: env('MAIL_TEST_EMAIL'),
+      },
+  },
+});
