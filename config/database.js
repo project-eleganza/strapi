671aac7b1430d5271cac35e9d2350dbb6fbdc0cc
Maria'lena Pietri <marialena.pietri@maps-system.com>
2022-02-08 21:34:55 +0100

    update bdd credentials

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'server'),
         username: env('DATABASE_USERNAME', 'eleganza'),
-        password: env('DATABASE_PASSWORD', 'b9DmLCpHR8mxD2w9'),
+        password: env('DATABASE_PASSWORD', 'oW599iA9!rc'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
