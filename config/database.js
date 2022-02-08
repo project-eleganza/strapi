d0319f825505455adc88d53507222dd726af6da7
Maria'lena Pietri <marialena.pietri@maps-system.com>
2022-02-08 21:39:18 +0100

    update bdd credentials

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.2zite.mongodb.net'),
+        host: env('DATABASE_HOST', 'cluster0.pyen5.mongodb.net'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'server'),
-        username: env('DATABASE_USERNAME', 'eleganza'),
-        password: env('DATABASE_PASSWORD', 'oW599iA9!rc'),
+        username: env('DATABASE_USERNAME', 'marialena'),
+        password: env('DATABASE_PASSWORD', 'MonMongodbAMoi82'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
