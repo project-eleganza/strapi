6e5e32b757fdac134860332f3068cea90bf86ff4
Maria'lena Pietri <marialena.pietri@maps-system.com>
2022-02-08 22:12:18 +0100

    update bdd credentials

--- config/database.js
+++ config/database.js
@@ -1,15 +1,21 @@
+const BDD_HOST = process.env.DATABASE_HOST;
+const BDD_PORT = process.env.DATABASE_PORT;
+const BDD_NAME = process.env.DATABASE_NAME;
+const BDD_USERNAME = process.env.DATABASE_USERNAME;
+const BDD_PASSWORD = process.env.DATABASE_PASSWORD;
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.pyen5.mongodb.net'),
+        host: BDD_HOST,
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'server'),
-        username: env('DATABASE_USERNAME', 'marialena'),
-        password: env('DATABASE_PASSWORD', 'MonMongodbAMoi82'),
+        port: BDD_PORT,
+        database: BDD_NAME,
+        username: BDD_USERNAME,
+        password: BDD_PASSWORD,
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
